4ba7a2809a00d83bb52574fced679298e932a20a
lea175 <55576593+lea175@users.noreply.github.com>
2022-12-27 01:54:39 +0100

    added the user

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const grid = document.querySelector('.grid')
-//const block = document.querySelector('.block')
 const blockWidth = 100 
 const blockHeight = 20
-const blocks = []
+const userStartPosition = [260,20]
+let userCurrentPosition = userStartPosition
+
+
 
 class Block {
     constructor(xAxis, yAxis) {
@@ -13,10 +15,37 @@ class Block {
     }
 }
 
-blocks = [
-    new Block(20, 370)
+let blocks = [
+    new Block(20, 370),
+    new Block(140, 370),
+    new Block(260, 370),
+    new Block(380, 370),
+    new Block(500, 370),
+    new Block(20, 320),
+    new Block(140, 320),
+    new Block(260, 320),
+    new Block(380, 320),
+    new Block(500, 320),
+    new Block(20, 270),
+    new Block(140, 270),
+    new Block(260, 270),
+    new Block(380, 270),
+    new Block(500, 270),
 ]
 
-function drawBlocks {
-    
+function drawBlocks() {
+    for(i=0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block) 
+    }
+//create user block
+const user = document.createElement('div')
+user.classList.add('user')
+user.style.left = userCurrentPosition[0] + 'px'
+user.style.bottom = userCurrentPosition[1] + 'px'
+grid.appendChild(user)
 }
+drawBlocks()
